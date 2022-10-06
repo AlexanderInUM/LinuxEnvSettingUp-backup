d211879b222911a565865e7c5e8f0b5b4bbfe9e6
AlexanderInUM <alexander.um.edu@gmail.com>
2022-10-06 09:54:46 -0700

    auto commit at ostime(075286) via gitPusher

--- env.sh
+++ env.sh
@@ -11,9 +11,14 @@ sudo apt install -y golang
 sudo apt install -y python
 sudo apt install -y pip
 sudo apt install -y lua5.3
+sudo apt install -y docker.io
+sudo apt install -y geany
+sudo apt install -y meld
 sudo apt install -y nodejs
 sudo apt install -y npm
 sudo apt install -y baobab
+sudo apt install -y ffmpeg
+sudo apt install -y mlocate
 #Start: LAMP Installation
 sudo apt install -y php
 sudo apt install -y cmatrix
@@ -22,10 +27,17 @@ sudo apt install -y apache2
 #echo '<?php phpinfo(); ?>' | sudo tee -a /var/www/html/phpinfo.php > /dev/null #Create a phpinfo.php test page
 #sudo rm /var/www/html/phpinfo.php #Once you’ve confirmed that PHP is working correctly, delete the test file.
 # visit http://127.0.0.1/ to see the effect, the file is under `/var/html` folder
+sudo apt install -y redis
+#redis-cli --version
+#sudo systemctl status redis
+#sudo systemctl start redis-server
+#sudo systemctl stop redis
 sudo apt install mariadb-server -y mariadb-client
 #sudo systemctl status mariadb #Once installed, check it’s running correctly.
 #sudo mysql_secure_installation #Secure your newly installed MariaDB service.
 #End
+sudo apt install -y rpm
+sudo apt install -y zip
 sudo aptitude install -y clang
 sudo aptitude install -y g++
 sudo aptitude install -y build-essential
@@ -38,7 +50,10 @@ python3 -c "import cv2; print(cv2.__version__)"
 sudo npm install -g servez
 sudo npm install -g cryptee
 sudo npm install -g splitee
+sudo npm install -g wikit  #search wikipidea by wikit TARGET
 
+# Other binaries
+# https://www.anaconda.com/products/distribution #conda
 
 
 
